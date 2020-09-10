7818d2f3c06b895a094798ef6b7efb581fb77992
Nahuel Bigurrarena <nahuel_bigu@hotmail.com>
2020-09-10 19:00:47 -0300

    Snack

---
+++ Clase1/Snack.py
@@ -0,0 +1,11 @@
+from sys import stdin, stdout
+
+# Collection of products
+products = {1: 4.00, 2:4.50, 3:5.00, 4:2.00 , 5:1.50}
+
+# Lectura de datos
+n=2
+productCode, quantityProduct = [int(n) for n in stdin.readline().strip().split()]
+
+# Imprimir
+stdout.write("Total: R$ {:.2f}".format(products[productCode]*quantityProduct) + '\n')
