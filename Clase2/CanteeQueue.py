4498e7073dcddb6a646971fab45584b6880c5044
BIGURRARENA, NAHUEL <nahuel_bigu@hotmail.com>
2020-09-21 16:17:19 -0300

    Cantee

--- Clase2/CanteeQueue.py
+++ Clase2/CanteeQueue.py
@@ -6,10 +6,8 @@ cases=int(stdin.readline().strip())
 for case in range(cases):
     studentsNro=int(stdin.readline().strip())
     students=[int(n) for n in stdin.readline().strip().split()]
-    count=0
     grades=list(enumerate(students))
     order=grades[:]    
     order.sort(key = lambda x: x[1],reverse=True)
     order=list(filter(lambda x: x[0]==order[x[0]][0] ,grades))
-    print (order)
     stdout.write(str(len(order))+"\n")
